be995fba70251e5de96aaf2dd290b6b1762e6d1e
tjsalsu <tjsalsu@naver.com>
2022-10-05 01:53:44 +0900

    마이페이지(프로필,환경설정)

---
+++ app/src/main/java/ddwucom/mobile/test13/mydata2/SettingActivity.java
@@ -0,0 +1,14 @@
+package ddwucom.mobile.test13.mydata2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SettingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+    }
+}
